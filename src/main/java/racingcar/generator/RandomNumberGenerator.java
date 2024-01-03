bc40a3cb230d85ccc2e9c29bb0cb263c910ad210
Jeongns <jhlee030313@gmail.com>
2024-01-03 23:54:49 +0900

    refactor(numberGenerator): 난수 생성기 로직 리펙토링

--- src/main/java/racingcar/generator/RandomNumberGenerator.java
+++ src/main/java/racingcar/generator/RandomNumberGenerator.java
@@ -4,8 +4,12 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
+    private final int START_NUMBER = 1;
+    private final int END_NUMBER = 9;
+
+
     @Override
-    public int generate(int startNumber, int endNumber) {
-        return pickNumberInRange(startNumber, endNumber);
+    public int generate() {
+        return pickNumberInRange(START_NUMBER, END_NUMBER);
     }
 }
