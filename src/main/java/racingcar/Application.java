77f874c6ad87cc58df1b788e9bc292e36921b7e4
Jeongns <jhlee030313@gmail.com>
2024-02-05 22:11:44 +0900

    feat(Application): 레이싱 게임 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,35 @@
 package racingcar;
 
+import racingcar.console.Printer;
+import racingcar.console.Reader;
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingGame;
+import racingcar.generator.RandomNumberGenerator;
+
+import java.util.List;
+
+import static racingcar.console.Printer.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Printer printer = new Printer();
+        Reader reader = new Reader();
+        RacingGame racingGame = new RacingGame(new RandomNumberGenerator());
+
+
+        printer.print(START_MESSAGE);
+        List<String> names = reader.readNames();
+        racingGame.joinRace(names);
+
+        printer.print(TRY_MESSAGE);
+        int tryCount = reader.readNumber();
+
+        for(int i = 0; i < tryCount; i++){
+            racingGame.moveCarsForward();
+            printer.printCurrentStatus(racingGame.getCurrentStatus());
+        }
+
+        List<RacingCar> winners = racingGame.getWinners();
+        printer.printWinners(winners);
     }
 }
